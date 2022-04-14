b4150e458acd7784038a8f06a611377e199aa131
thanhphung2 <thanhpdph18767@fpt.edu.vn>
2022-04-14 21:22:37 +0700

    update

--- mvc/view/client/header.php
+++ mvc/view/client/header.php
@@ -9,7 +9,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link rel="stylesheet" href="./style.css"> -->
     <link rel="stylesheet" href="./css/bootstrap.css">
     <link rel="stylesheet" href="./css/product.css">
     <link rel="stylesheet" href="./css/cart.css">
@@ -198,78 +197,3 @@
             </div>
         </div>
     </div>
-    <!-- <div class="nav-control">
-        <div class="container">
-            <div class="logo">
-                <img src="./img/logo.png" alt="">
-            </div>
-            <div class="menu">
-                <ul>
-                    <li><a href="../../../index.php">Trang chủ</a></li>
-                    <li><a href="#">Giới thiệu</a></li>
-                    <li class="menu_link"><a href="./controller_view.php">Sản phẩm &#8744;</a>i
-                        <ul class="menu_dow">
-                            <table>
-                                <tr>
-                                    <th><a href="#">Coffee</a></th>
-                                    <th><a href="#">Nước ép</a></th>
-                                    <th><a href="#">Trà sữa</a></th>
-                                    <th><a href="#">Cocktail</a></th>
-
-                                </tr>
-                                <tr>
-                                    <td><a href="#">Esspero</a></td>
-                                    <td><a href="#">Dưa hấu</a></td>
-                                    <td><a href="#">Kiwi</a></td>
-                                    <td><a href="#">Cocktail B-52</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#">Esspero</a></td>
-                                    <td><a href="#">Dưa hấu</a></td>
-                                    <td><a href="#">Kiwi</a></td>
-                                    <td><a href="#">Cocktail B-52</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#">Esspero</a></td>
-                                    <td><a href="#">Dưa hấu</a></td>
-                                    <td><a href="#">Kiwi</a></td>
-                                    <td><a href="#">Cocktail B-52</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#">Esspero</a></td>
-                                    <td><a href="#">Dưa hấu</a></td>
-                                    <td><a href="#">Kiwi</a></td>
-                                    <td><a href="#">Cocktail B-52</a></td>
-                                </tr>
-                            </table>
-                        </ul>
-                    </li>
-                    <li><a href="#">Tin tức</a></li>
-                    <li><a href="#">Liên hệ</a></li>
-                </ul>
-            </div>
-            <div class="acount">
-                <div class="search">
-                    <form action="controller_view.php?act=product" method="post">
-                        <input type="text" name="kyw" placeholder="Tìm kiếm">
-                        <input type="submit" name="timkiem" value="Tìm kiếm">
-                    </form>
-                </div>
-                <div class="user">
-                    <i class='bx bxs-user' style='color:#fff9f9'></i>
-                    <div class="login-form">
-                        <a href="#">Đăng nhập</a>
-                        <a href="#">Đăng kí</a>
-                    </div>
-                </div>
-                <div class="cart">
-                    <a href="./controller_view.php?act=addtocart"><i class='bx bxs-cart' style='color:#ffffff'>
-                            <div class="boxcart" id="boxcart">
-                                <!-- <span id="carticon" class="badge bg-danger rounded-pill"><?=$slsp?></span> -->
-                            </div>
-                        </i></a>
-                </div>
-            </div>
-        </div>
-
-    </div> -->
